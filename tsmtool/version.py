dc630fefe3af6433a89785f81b4cedcd8eb40cda
Matt Krueger <mkrueger@rstms.net>
2022-03-28 08:23:04 -0500

    Bump version: 1.0.4 → 1.1.0

--- tsmtool/version.py
+++ tsmtool/version.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-03-28 06:54:53-05:00"
+__timestamp__ = "2022-03-28 08:23:04-05:00"
